95c6dfc95501e7047a4c64ca334b1a4205137e4a
Sergio <semartinl@alumnos.unex.es>
2023-02-07 21:12:05 +0100

    Movimientos del robot

--- marica.py
+++ marica.py
@@ -1,5 +1,58 @@
 import sys
-!{sys.executable} -m pip install --user numpy
 import numpy as np
-def square(x):
-    return x * x
+
+def move_up(board):
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            if board[x, y] == 1:
+                if x > 0:
+                    board[x, y] = 0
+                    board[x - 1, y] = 1
+                    return
+                else:
+                    print("Movimiento invalido")
+
+def move_down(board):
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            if board[x, y] == 1:
+                if x < board.shape[0] - 1:
+                    board[x, y] = 0
+                    board[x + 1, y] = 1
+                    return
+                else:
+                    print("Movimiento invalido")
+
+def move_left(board):
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            if board[x, y] == 1:
+                if y > 0:
+                    board[x, y] = 0
+                    board[x, y - 1] = 1
+                    return
+                else:
+                    print("Movimiento invalido")
+
+def move_right(board):
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            if board[x, y] == 1:
+                if y < board.shape[1] - 1:
+                    board[x, y] = 0
+                    board[x, y + 1] = 1
+                    return
+                else:
+                    print("Movimiento invalido")
+
+tablero = np.zeros((12, 12))
+tablero[0, 0] = 1
+
+print(tablero)
+move_down(tablero)
+print(tablero)
+move_right(tablero)
+print(tablero)
+move_up(tablero)
+print(tablero)
+move_left(tablero)
