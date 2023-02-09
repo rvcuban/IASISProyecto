ac200778e099678a1502ef9123d507ba322f8bff
rvcuban <jrodrigufxj@alumnos.unex.es>
2023-02-09 21:20:41 +0100

    movimientos del robot y recoger monedas hecho

--- FuncionesHeuristica.py
+++ FuncionesHeuristica.py
@@ -1,51 +1,149 @@
 import numpy as np
+
+
 def localizarObjetivo(tablero, num):
     for x, y in zip(*np.where(tablero == num)):
-        print("El elemento a[{}, {}] vale {}".format(x, y, tablero[x,y]))
+        print("El elemento buscado[{}, {}] vale {}".format(x, y, tablero[x, y]))
         break
-    return x,y
-class tablero:
-    def __init__(self, monedas, posicionRobotX, posicionRobotY,table):
-        self.monedas=monedas
-        self.posicionRobotX= posicionRobotX
-        self.posicionRobotY= posicionRobotY
+    return x, y
+
+
+class Tablero:
+    """esta es la clase tablero que almacenara toda la infomracion del juego"""
+
+    def __init__(self, monedas, posicionRobotX, posicionRobotY, table):
+        if not monedas:
+            return None
+        self.monedas = monedas
+        self.posicionRobotX = posicionRobotX
+        self.posicionRobotY = posicionRobotY
         self.table = table
-    
+
+        # agregado
+        self.monedasRecogidas = 0
+
+    def __str__(self) -> str:
+        return f"Numero de monedas:{self.monedas} en el tablero\n {self.table}\n posicion del robot:{self.posicionRobotX,self.posicionRobotY}\n el numero de monedas recogidas actualmente es {self.monedasRecogidas}"
 
 
+def movValid(tablero: Tablero, x, y):
+    if tablero.table[x, y] != 9:
+        return True
 
-def move_up(board):
-    for x in range(board.shape[0]):
-        for y in range(board.shape[1]):
-            if board[x, y] == 1:
-                if x > 0:
-                    board[x, y] = 0
-                    board[x - 1, y] = 1
-                    return
-                else:
-                    print("Movimiento invalido")
 
-    
+def hayMoneda(tablero: Tablero, x, y):
+    if tablero.table[x, y] < 7:
+        valorMoneda = tablero.table[x, y]
+        m = True
+        return (m, valorMoneda)
+    else:
+        m = False
+        return m
+
+
+def move_up(tablero: Tablero):
+    x = tablero.posicionRobotX
+    y = tablero.posicionRobotY
+
+    if movValid(tablero, x - 1, y):
+        exisMoneda, valorMoneda = hayMoneda(tablero, x - 1, y)
+        if (
+            exisMoneda
+        ):  # monedas de 0 almacena true o false para saber si hay moneda en la casilla que nos movemos
+            tablero.monedasRecogidas = (
+                tablero.monedasRecogidas + valorMoneda
+            )  # modenas[1] almacena el valor
+
+        tablero.posicionRobotX = tablero.posicionRobotX - 1
+        tablero.posicionRobotY = tablero.posicionRobotY
+        tablero.table[x - 1, y] = 8
+        tablero.table[x, y] = 0
+    else:
+        print("movimineto invalido;el robot permanece quieto")
+
+
+def move_down(tablero: Tablero):
+    x = tablero.posicionRobotX
+    y = tablero.posicionRobotY
+
+    if movValid(tablero, x + 1, y):
+        exisMoneda, valorMoneda = hayMoneda(tablero, x + 1, y)
+        if (
+            exisMoneda
+        ):  # monedas de 0 almacena true o false para saber si hay moneda en la casilla que nos movemos
+            tablero.monedasRecogidas = (
+                tablero.monedasRecogidas + valorMoneda
+            )  # modenas[1] almacena el valor
+
+        tablero.posicionRobotX = tablero.posicionRobotX + 1
+        tablero.posicionRobotY = tablero.posicionRobotY
+        tablero.table[x + 1, y] = 8
+        tablero.table[x, y] = 0
+    else:
+        print("movimineto invalido;el robot permanece quieto")
+
+
+def move_left(tablero: Tablero):
+    x = tablero.posicionRobotX
+    y = tablero.posicionRobotY
+
+    if movValid(tablero, x, y - 1):
+        exisMoneda, valorMoneda = hayMoneda(tablero, x, y - 1)
+        if (
+            exisMoneda
+        ):  # monedas de 0 almacena true o false para saber si hay moneda en la casilla que nos movemos
+            tablero.monedasRecogidas = (
+                tablero.monedasRecogidas + valorMoneda
+            )  # modenas[1] almacena el valor
+
+        tablero.posicionRobotX = tablero.posicionRobotX
+        tablero.posicionRobotY = tablero.posicionRobotY - 1
+        tablero.table[x, y - 1] = 8
+        tablero.table[x, y] = 0
+    else:
+        print("movimineto invalido;el robot permanece quieto")
+
+
+def move_right(tablero: Tablero):
+    x = tablero.posicionRobotX
+    y = tablero.posicionRobotY
+
+    if movValid(tablero, x, y + 1):
+        exisMoneda, valorMoneda = hayMoneda(tablero, x, y + 1)
+        if (
+            exisMoneda
+        ):  # monedas de 0 almacena true o false para saber si hay moneda en la casilla que nos movemos
+            tablero.monedasRecogidas = (
+                tablero.monedasRecogidas + valorMoneda
+            )  # modenas[1] almacena el valor
+
+        tablero.posicionRobotX = tablero.posicionRobotX
+        tablero.posicionRobotY = tablero.posicionRobotY + 1
+        tablero.table[x, y + 1] = 8
+        tablero.table[x, y] = 0
+    else:
+        print("movimineto invalido;el robot permanece quieto")
+
+
 def moverArriba(tablero):
     posRobotX = tablero.posicionRobotX
     posRobotY = tablero.posicionRobotY
-    if tablero.table[posRobotX][posRobotY-1] == 9:
+    if tablero.table[posRobotX][posRobotY - 1] == 9:
         return tablero
-    
-    if tablero.table[posRobotX][posRobotY-1] == 7:
-        tablero.table[posRobotX][posRobotY-1] = 8
+
+    if tablero.table[posRobotX][posRobotY - 1] == 7:
+        tablero.table[posRobotX][posRobotY - 1] = 8
         tablero.table[posRobotX][posRobotY] = 0
         return tablero
-    if tablero.table[posRobotX][posRobotY-1] == range(1,6):
-        tablero.table[posRobotX][posRobotY-1] = 8
+    if tablero.table[posRobotX][posRobotY - 1] == range(1, 6):
+        tablero.table[posRobotX][posRobotY - 1] = 8
         tablero.table[posRobotX][posRobotY] = 0
         return tablero
-    if tablero.table[posRobotX][posRobotY-1] == 7:
-        tablero.table[posRobotX][posRobotY-1] = 8
+    if tablero.table[posRobotX][posRobotY - 1] == 7:
+        tablero.table[posRobotX][posRobotY - 1] = 8
         tablero.table[posRobotX][posRobotY] = 0
         return tablero
-    if tablero.table[posRobotX][posRobotY-1] == 0:
-        tablero.table[posRobotX][posRobotY-1] = 8
+    if tablero.table[posRobotX][posRobotY - 1] == 0:
+        tablero.table[posRobotX][posRobotY - 1] = 8
         tablero.table[posRobotX][posRobotY] = 0
         return tablero
-    
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-
 import numpy as np
 import FuncionesHeuristica as heur
-def cargarArchivo():
+def cargarArchivo():       #falta lanzar error en caso de que el archivo no exista o no este en la carpeta 
     with open('LABECOIN1.txt','r') as f:
+        monedas=next(f)
         datos = ''.join(f.readlines()).replace('\n',';')
     m = np.matrix(datos)
 
-    return m
+    return (m,monedas)
+ 
 
 def main():
     print("LABECOIN")
-    labecoin = cargarArchivo()
-
-
+    labecoin,monedas = cargarArchivo()  #tenemos el mata y el numero de modenas 
+    #despues de cargar el archivo vamos a localizar la posicion de los elementos y crearel tablero
+    print("ESTE ES EL MAPA QUE SE VA A RESOLVER")
+    print(f"El numero de monedas es {monedas}")
+    print(labecoin)
+    posRobot=heur.localizarObjetivo(labecoin, 8) 
     # Source: https://stackoverflow.com/questions/47432043
 
     print("¿Con que algoritmo quieres resolver el puzzle?")
@@ -24,9 +28,16 @@ def main():
         case 2:
             print("Aqui falta Maxima pendiente")
     
-    posRobot=heur.localizarObjetivo(labecoin, 7)
-    tablero = heur.tablero(0, posRobot[0],posRobot[1], labecoin)
     
-    heur.moverArriba(tablero)
+    
+    tablero = heur.Tablero(monedas, posRobot[0],posRobot[1], labecoin)
+    
+    heur.move_up(tablero)
+    heur.move_right(tablero)
+    heur.move_up(tablero)
+    heur.move_right(tablero)
+    heur.move_right(tablero)
     print(tablero)
+
+  
 main()
